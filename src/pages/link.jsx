ac34dd1f1c38f1d23fbd3f1e1319daaaa49020b2
Ch.Eswar <150889483+Eswarchinthakayala-webdesign@users.noreply.github.com>
2025-07-19 18:43:14 +0530

    Link Updation

--- src/pages/link.jsx
+++ src/pages/link.jsx
@@ -76,12 +76,12 @@ const LinkPage = () => {
             {url?.title}
           </span>
           <a
-            href={`https://trimrr.vercel.app/${link}`}
+            href={`https://trimrr-sooty.vercel.app/${link}`}
             target="_blank"
             className="text-3xl sm:text-4xl text-blue-400 font-bold hover:underline cursor-pointer
             inline-block break-words whitespace-normal max-w-full"
           >
-            https://trimrr.vercel.app/{link}
+            https://trimrr-sooty.vercel.app/{link}
           </a>
           <a
             href={url?.original_url}
@@ -98,7 +98,7 @@ const LinkPage = () => {
             <Button
               variant="ghost"
               onClick={() =>
-                navigator.clipboard.writeText(`https://trimrr.in/${link}`)
+                navigator.clipboard.writeText(`https://trimrr-sooty.vercel.app/${link}`)
               }
             >
               <Copy />
